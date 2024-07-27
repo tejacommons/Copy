5111259945abaa4a26dde41213ffc95f29221054
tejacommons <tejacommons@gmail.com>
2024-07-27 21:48:18 +0530

    v8=>Handeled empty spaces and Implemented Obstacles response Feature

--- popup.js
+++ popup.js
@@ -40,10 +40,12 @@ document.getElementById('ticketDropdown').addEventListener('change', () => {
     const repetitiveOption = document.getElementById('repetitiveOption');
     const operatorResponseOption = document.getElementById('operatorResponseOption');
     const localizationLostOptions = document.getElementById('localizationLostOptions');
+    const playbackCheckOption=document.getElementById('playbackCheckOption');
 
     // Reset visibility of options
     repetitiveOption.style.display = 'block';
     operatorResponseOption.style.display = 'block';
+    playbackCheckOption.style.display='none';
     localizationLostOptions.style.display = 'none'; // Hide localization lost options by default
 
     if (selectedTicket === "False Cliff Detection") {
@@ -60,8 +62,9 @@ document.getElementById('ticketDropdown').addEventListener('change', () => {
     if (selectedTicket === "False Cliff Detection") {
         repetitiveOption.style.display = 'none'; // Hide Is This Issue Repetitive?
         operatorResponseOption.style.display = 'none'; // Hide Did Operator Respond?
-    } else if(selectedTicket === "Front Camera Noise"){
-        repetitiveOption.style.display='block';
+    }
+    else if(selectedTicket === "Front Camera Noise"){
+        repetitiveOption.style.display='none';
         document.getElementById('issueRepetitiveNoNeed').checked=true;
         document.getElementById('operatorRespondNo').checked = true;
     }else if (selectedTicket.includes('Localization Lost')) {
@@ -78,6 +81,8 @@ document.getElementById('ticketDropdown').addEventListener('change', () => {
     } else if(selectedTicket == 'Safety Monitor Failures: Vacuum Failure' ){
         document.getElementById('issueRepetitiveYes').checked = true;
         document.getElementById('operatorRespondNo').checked = true;
+    }else if(selectedTicket==='Safety Monitor Failures: Front/Back wheel encoder data integrity check failure'||selectedTicket==='Safety Monitor Failures: Velocity does not match the commanded velocity'||selectedTicket==='Safety Monitor Failures: Steering angle does not match the commanded angle'){
+        playbackCheckOption.style.display='block';
     }
     else {
         localizationLostOptions.style.display = 'none'; // Hide localization lost options
@@ -96,6 +101,8 @@ document.getElementById('copyButton').addEventListener('click', () =>
     const operatorRespondNoNeed = document.getElementById('operatorRespondNoNeed');
     const callResponseYes=document.getElementById('callResponseYes');
     const callResponseNo=document.getElementById('callResponseNo');
+    const playbackCheckYes=document.getElementById('playbackCheckYes');
+    const playbackCheckNo=document.getElementById('playbackCheckNo');
     let textToCopy = '';
 
     if (fruitDropdown.style.display !== 'none') {
@@ -122,27 +129,34 @@ document.getElementById('copyButton').addEventListener('click', () =>
     }
         let repetitiveResponse = '';
         let operatorResponse = '';
+        let playbackresponse='';
         let additionalObservations = '';
 
         if (issueRepetitiveYes.checked) {
-            repetitiveResponse = "RA try to reset it but it's repetitive";
+            repetitiveResponse = "RA try to reset it but it's repetitive\n";
         } else if (issueRepetitiveNo.checked) {
-            repetitiveResponse = "RA reset it, neo continued cleaning";
+            repetitiveResponse = "RA reset it, neo continued cleaning\n";
         } else {
             repetitiveResponse = "";
         }
 
         if (operatorRespondYes.checked) {
-            operatorResponse = "RA called operator and they will assist";
+            operatorResponse = "RA called operator and they will assist\n";
         } else if (operatorRespondNo.checked) {
-            operatorResponse = "RA called operator and ended in no response";
+            operatorResponse = "RA called operator and ended in no response\n";
         } else {
-            operatorResponse = " ";
+            operatorResponse = "";
+        }
+
+        if(playbackCheckYes.checked){
+            playbackresponse="RA noticed neo passed over some objects/uneven surface\n"
+        }else{
+            playbackresponse="";
         }
 
         let cancelUpdate;
             if((issueRepetitiveYes.checked ||issueRepetitiveNoNeed.checked) && operatorRespondNo.checked){
-                cancelUpdate= `RA cancel CP After 15 min`;
+                cancelUpdate= `RA cancel CP After 15 min\n`;
             }else{cancelUpdate=``}
         
 
@@ -159,23 +173,23 @@ document.getElementById('copyButton').addEventListener('click', () =>
                 additionalObservations += `${option.value}\n`;
             });
             textToCopy=`
-            ${sectorInput}'\n ${repetitiveResponse}\n${additionalObservations}\n${operatorResponse}\n${cancelUpdate}`
+            ${sectorInput}${repetitiveResponse}${additionalObservations}\n${operatorResponse}${cancelUpdate}`
         } else if (selectedTicket === "False Cliff Detection") {
             textToCopy = `
                 ${sectorInput} presenting\nFalse cliff detection\nRA tried giving manual moves but it's still there and neo can't able to plan path\nRA cancel the CP\n3D-Diagnostics:-`;
         } else if(selectedTicket === "Front Camera Noise"){
-            textToCopy =`${sectorInput} presenting\n Front Camera Noise\nRA tried giving manual moves but it's still there and neo can't able to plan path\n${operatorResponse}\n${cancelUpdate}\n3D-Diagnostics:-`
+            textToCopy =`${sectorInput} presenting\n Front Camera Noise\nRA tried giving manual moves but it's still there and neo can't able to plan path\n${operatorResponse}${cancelUpdate}3D-Diagnostics:-`
         }
         else if (selectedTicket === "Safety Monitor Failures: Squeegee Detatch (Rear Manual Moves)") {
             const trimmedTicket = selectedTicket.slice(0, -19);
             textToCopy = `
-                ${sectorInput}, RA took the control,RA noticed there is Tight space\nRA tried giving Rear manual move and neo started presenting the following:\n${trimmedTicket}\n${repetitiveResponse}\n${operatorResponse}\n${cancelUpdate}`;
+                ${sectorInput}, RA took the control,RA noticed there is Tight space\nRA tried giving Rear manual move and neo started presenting the following:\n${trimmedTicket}\n${repetitiveResponse}${operatorResponse}${cancelUpdate}`;
         } else if (selectedTicket === "Safety Monitor Failures: Squeegee Detatch (Stuck By Obstacles)") {
             const trimmedTicket = selectedTicket.slice(0, -20);
             textToCopy = `
-                ${sectorInput}, RA took the control\nRA noticed there is Tight space\nRA tried giving manual move and neo started presenting the following:\n${trimmedTicket}\n${repetitiveResponse}\n${operatorResponse}\n${cancelUpdate}`;
+                ${sectorInput}, RA took the control\nRA noticed there is Tight space\nRA tried giving manual move and neo started presenting the following:\n${trimmedTicket}\n${repetitiveResponse}${operatorResponse}${cancelUpdate}`;
         } else {
-            textToCopy = `${sectorInput} presented the following:\n${selectedTicket}\n${repetitiveResponse}\n${operatorResponse}\n${cancelUpdate}\nDiagnostics:- `;
+            textToCopy = `${sectorInput} presented the following:\n${selectedTicket}\n${repetitiveResponse}${playbackresponse}${operatorResponse}${cancelUpdate}Diagnostics:- `;
         }
     }
 
